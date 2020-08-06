d6d52ef4ae9acd7cfd366c4065790862ea5f969d
sytone <github@sytone.com>
2020-08-06 11:49:03 -0700

    Update release script to validte release

--- release.ps1
+++ release.ps1
@@ -14,3 +14,42 @@ Write-Host "Tagging head with v$($moduleManifest.version)"
 git tag "v$($moduleManifest.version)" -m "Release v$($moduleManifest.version)" -e
 Write-Host "Pushing tag for build"
 git push origin "v$($moduleManifest.version)"
+git push
+
+$released = $false
+while (-not $released) {
+    (Invoke-WebRequest $moduleManifest.manifest).Content | Set-Content "$env:TMP\module.temp.json" -NoNewLine -Force
+    if ((Get-Content .\module.json -Raw).Replace("`r`n", "`n") -eq (Get-Content "$env:TMP\module.temp.json" -Raw)) {
+        Write-Host "Release completed, validating."
+    } 
+    else { 
+        Write-Host "." -NoNewline
+        Start-Sleep -Seconds 5
+    }    
+}
+
+$moduleManifest = (Get-Content .\module.json -Raw | ConvertFrom-Json -Depth 10)
+
+if((Invoke-WebRequest $moduleManifest.manifest).StatusCode -eq 200) {
+    Write-Host "Manifest valid."
+} else {
+    Write-Host "Issue with manifest." -ForegroundColor Red
+}
+
+if((Invoke-WebRequest $moduleManifest.download).StatusCode -eq 200) {
+    Write-Host "download valid."
+} else {
+    Write-Host "Issue with download." -ForegroundColor Red
+}
+
+if((Invoke-WebRequest $moduleManifest.readme).StatusCode -eq 200) {
+    Write-Host "readme valid."
+} else {
+    Write-Host "Issue with readme." -ForegroundColor Red
+}
+
+if((Invoke-WebRequest $moduleManifest.changelog).StatusCode -eq 200) {
+    Write-Host "changelog valid."
+} else {
+    Write-Host "Issue with changelog." -ForegroundColor Red
+}
